afe77ef09864cba59972b9c24eaa7478f93a8a31
cyberdrish <drishmalhotra1997@gmail.com>
2025-03-28 03:19:37 +0530

    cancelToken

--- src/hook/useAxiosFetch.js
+++ src/hook/useAxiosFetch.js
@@ -1,5 +1,5 @@
 // useAxiosFetch.js
-import { useState } from "react";
+import { useRef, useState } from "react";
 import axios from "axios";
 
 const useAxiosFetch = (
@@ -10,13 +10,24 @@ const useAxiosFetch = (
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const cancelTokenSourceRef = useRef(null);
+
   const fetchAll = (query) => {
     if (!url) return;
+
+    if (cancelTokenSourceRef.current) {
+      cancelTokenSourceRef.current.cancel("Canceled due to new request");
+    }
+
+    cancelTokenSourceRef.current = axios.CancelToken.source();
+
     setData([]);
     setError(null);
     setIsLoading(true);
     axios
-      .get(url + query)
+      .get(url + query, {
+        cancelToken: cancelTokenSourceRef.current.token,
+      })
       .then((res) => {
         // console.log(res.data);
         setData(res.data);
