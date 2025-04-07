ebdc48620659c71862ecfee68b09a31458c6af12
cyberdrish <drishmalhotra1997@gmail.com>
2025-04-07 23:05:51 +0530

    info

--- src/hook/useAxiosFetch.js
+++ src/hook/useAxiosFetch.js
@@ -3,9 +3,7 @@ import { useRef, useState } from "react";
 import axios from "axios";
 
 const useAxiosFetch = (
-  url = `http://www.omdbapi.com/?apikey=${
-    import.meta.env.VITE_MOVIE_API_KEY
-  }&s=`
+  url = `http://www.omdbapi.com/?apikey=${import.meta.env.VITE_MOVIE_API_KEY}&`
 ) => {
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
@@ -13,25 +11,28 @@ const useAxiosFetch = (
   const cancelTokenSourceRef = useRef(null);
 
   const fetchAll = (query) => {
-    const url = new URL(
-      `http://www.omdbapi.com/?apikey=${
-        import.meta.env.VITE_MOVIE_API_KEY
-      }&${new URLSearchParams({ s: query }).toString()}`
-    );
     if (!url) return;
+    const fetchUrl = new URL(`${url}${new URLSearchParams({ s: query })}`);
+
+    // or can use below code as it is a single value
+    // const fetchUrl = new URL(
+    //   `http://www.omdbapi.com/?apikey=${
+    //     import.meta.env.VITE_MOVIE_API_KEY
+    //   }&s=${encodeURIComponent(query)}`
+    // );
 
     if (cancelTokenSourceRef.current) {
       cancelTokenSourceRef.current.cancel("Canceled due to new request");
     }
 
     cancelTokenSourceRef.current = axios.CancelToken.source();
-    console.log(url);
+    console.log(fetchUrl);
 
     setData([]);
     setError(null);
     setIsLoading(true);
     axios
-      .get(url, {
+      .get(fetchUrl, {
         cancelToken: cancelTokenSourceRef.current.token,
       })
       .then((res) => {
